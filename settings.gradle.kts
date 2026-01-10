1fc6510ac11553b9873995df114e562daa224c39
Latitude 7420 <amorusnn@gmail.ru>
2026-01-10 21:14:36 +0600

    added login

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,6 +21,22 @@ dependencyResolutionManagement {
 
 rootProject.name = "Test task  for Effective Mobile"
 include(":app")
-include(":core-ui")
-include(":core-network")
-include(":core-database")
+
+
+include(":core:core-ui")
+include(":core:core-network")
+include(":core:core-database")
+include(":core:core-utils")
+
+include(":feature:auth:auth-ui")
+include(":feature:auth:auth-domain")
+include(":feature:auth:auth-api")
+
+include(":feature:courses:courses-ui")
+include(":feature:courses:courses-domain")
+include(":feature:courses:courses-api")
+
+include(":feature:favorites:favorites-ui")
+include(":feature:favorites:favorites-domain")
+
+include(":feature:account:account-ui")
