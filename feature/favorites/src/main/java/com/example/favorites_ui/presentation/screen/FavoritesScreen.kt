b81e96f51e00b0d1d61029aa859cf0ea8f5fa907
Latitude 7420 <amorusnn@gmail.ru>
2026-01-13 16:50:14 +0600

    almost ready

--- feature/favorites/src/main/java/com/example/favorites_ui/presentation/screen/FavoritesScreen.kt
+++ feature/favorites/src/main/java/com/example/favorites_ui/presentation/screen/FavoritesScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.favorites_ui.presentation.view_model.FavoritesViewModel
 import com.example.favorites_ui.widget.FavoriteCourseCard
 
@@ -61,7 +62,10 @@ fun FavoritesScreen(
                         Column(
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-
+                            Text(
+                                text = "⭐" ,
+                                fontSize = 48.sp
+                            )
                             Spacer(modifier = Modifier.height(16.dp))
                             Text(
                                 text = "Нет избранных курсов" ,
