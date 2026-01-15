e05d97530f6ecc1c6b16c6ea3038540d4225f8ed
Latitude 7420 <amorusnn@gmail.ru>
2026-01-15 22:35:46 +0600

    added new constant

--- core/common/src/main/java/com/example/common/constant/Constant.kt
+++ core/common/src/main/java/com/example/common/constant/Constant.kt
@@ -2,4 +2,8 @@ package com.example.common.constant
 
 object ApiConfig {
     const val BASE_URL = "https://drive.usercontent.google.com/"
+
+    const val VK_URL =  "https://vk.com/"
+    const val OK_URL =  "https://ok.ru/"
+
 }
