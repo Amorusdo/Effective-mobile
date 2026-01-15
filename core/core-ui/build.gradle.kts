12d783f6aa1324f817a569ef8f94c528adca7f3d
Latitude 7420 <amorusnn@gmail.ru>
2026-01-15 21:14:59 +0600

    new configure for Gradle module

--- core/core-ui/build.gradle.kts
+++ core/core-ui/build.gradle.kts
@@ -39,10 +39,14 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:core"))
+    implementation(project(":core:core-navigation"))
     implementation(libs.androidx.core.ktx)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.koin.androidx.compose)
 }
--- feature/navigation/build.gradle.kts
+++ feature/navigation/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.auth_ui"
+    namespace = "com.example.navigation"
     compileSdk = 36
 
     defaultConfig {
-        minSdk = 27
+        minSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -33,27 +33,23 @@ android {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
-    buildFeatures {
-        compose = true
+    android {
+        buildFeatures {
+            compose = true
+        }
     }
 }
 
 dependencies {
 
-    implementation(project(":core:core-ui"))
     implementation(project(":core:core"))
-    implementation(project(":core:common"))
-
+    
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.compose.material.icons.extended)
-
-
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.compose.runtime)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.koin.androidx.compose)
 }
