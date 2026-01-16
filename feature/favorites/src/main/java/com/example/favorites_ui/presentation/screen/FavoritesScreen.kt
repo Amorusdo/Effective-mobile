21e3f11d1258c1a51e11c0e2b63dbb539d857841
Latitude 7420 <amorusnn@gmail.ru>
2026-01-16 13:12:59 +0600

    remove hardcoded values

--- feature/favorites/src/main/java/com/example/favorites_ui/presentation/screen/FavoritesScreen.kt
+++ feature/favorites/src/main/java/com/example/favorites_ui/presentation/screen/FavoritesScreen.kt
@@ -20,11 +20,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.example.core_ui.theme.Typography
 import com.example.core_ui.theme.height16
 import com.example.core_ui.theme.padding16
